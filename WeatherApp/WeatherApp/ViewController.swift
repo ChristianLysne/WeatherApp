d9eb2347b10c162eed890bbbbce2493533071663
Christian Lysne <christian.lysne@gmail.com>
2016-07-02 12:48:07 +0200

    Created Xcode project

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Christian Lysne on 02/07/16.
+//  Copyright © 2016 Christian Lysne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
